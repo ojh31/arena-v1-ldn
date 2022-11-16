50099665a0e59b54a41a4efd313205de410430f7
ojh31 <oskar.hollinsworth@gmail.com>
2022-11-16 15:45:31 +0000

    Used nrepeats env variable

--- docker/hello.py
+++ docker/hello.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p>" * int(os.environ.get('NREPEATS', 1))
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5999))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
