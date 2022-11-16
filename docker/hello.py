55e28ad3be94e5212a40b1e22fce0b56b32da03e
ojh31 <oskar.hollinsworth@gmail.com>
2022-11-16 15:35:55 +0000

    Working conda env in docker using Joseph's code

--- docker/hello.py
+++ docker/hello.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>" * os.environ.get('NREPEATS', 1)
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5999))
+    app.run(debug=True, host='0.0.0.0', port=port)
